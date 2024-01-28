3f9599bf116aec817067e4a863e130c434765d1e
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-01-28 12:12:39 +0700

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X663D.mk
+    
+COMMON_LUNCH_CHOICES := \
+    twrp_X663D-user \
+    twrp_X663D-userdebug \
+    twrp_X663D-eng
---
+++ twrp_X663D.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from X663D device
+$(call inherit-product, device/infinix/X663D/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X663D
+PRODUCT_NAME := twrp_X663D
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := X663D
+PRODUCT_MANUFACTURER := Infinix
+
